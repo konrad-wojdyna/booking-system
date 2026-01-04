e4b75bd01c785b3b3df8d5b660d66f8cbadc6f67
konrad-wojdyna <konrad.wojdyna@gmail.com>
2026-01-04 22:14:50 +0100

    feat(security): add JWT authentication

---
+++ src/main/java/com/booking/bookingsystem/security/userdetails/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.booking.bookingsystem.security.userdetails;
+
+import com.booking.bookingsystem.model.User;
+import com.booking.bookingsystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(
+                "User not found: " + email
+        ));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole())))
+                .build();
+    }
+}
